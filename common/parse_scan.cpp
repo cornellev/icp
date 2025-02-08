1b803e43062cf9eebf2d2226bc215084aa195750
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-02-08 11:42:11 -0500

    Updated visualization to parse CSV-like files

--- common/parse_scan.cpp
+++ common/parse_scan.cpp
@@ -1,42 +1,46 @@
 #include "parse_scan.h"
+#include <charconv>
+#include <cmath>
+#include <cstddef>
+#include <fstream>
+#include <stdexcept>
+#include <string_view>
+#include <vector>
+#include "icp/geo.h"
 
-void parse_scan_var(const char* var, const char* data, void* user_data) {
-    LidarScan* scan = static_cast<LidarScan*>(user_data);
-    if (strcmp(var, "range_min") == 0) {
-        scan->range_min = strtod(data, NULL);
-    } else if (strcmp(var, "range_max") == 0) {
-        scan->range_max = strtod(data, NULL);
-    } else if (strcmp(var, "angle_max") == 0) {
-        scan->angle_max = strtod(data, NULL);
-    } else if (strcmp(var, "angle_min") == 0) {
-        scan->angle_min = strtod(data, NULL);
-    } else if (strcmp(var, "angle_increment") == 0) {
-        scan->angle_increment = strtod(data, NULL);
-    } else if (isdigit(var[0])) {
-        long index = strtol(var, NULL, 10);
-        double angle = scan->angle_min + index * scan->angle_increment;
-        double range = strtod(data, NULL);
-        if (range >= scan->range_min && range <= scan->range_max) {
-            scan->points.push_back(icp::Vector(100 * range * std::cos(angle),
-                100 * range * std::sin(angle)));
-        }
+float parse_float(std::string_view view) {
+    float f;
+    auto [_, ec] = std::from_chars(view.data(), (view.data() + view.size()), f);
+    if (ec != std::errc()) {
+        throw std::runtime_error("failed to read lidar scan: invalid value");
     }
+    return f;
 }
 
-LidarScan parse_lidar_scan(std::string path) {
-    FILE* file = fopen(path.c_str(), "r");
-    if (!file) {
-        perror("parse_config: fopen");
-        std::exit(1);
+std::vector<icp::Vector> parse_lidar_scan(std::string path) {
+    std::ifstream in(path);
+    if (!in.is_open()) {
+        throw std::runtime_error("failed to read lidar scan: failed to open file");
     }
 
-    LidarScan scan;
-    if (conf_parse_file(file, parse_scan_var, &scan) != 0) {
-        perror("parse_config: conf_parse_file");
-        std::exit(1);
+    std::vector<icp::Vector> result;
+
+    std::string line;
+    while (std::getline(in, line)) {
+        size_t index = line.find(',');
+        std::string_view view(line);
+
+        std::string_view x_view = view.substr(0, index);
+        std::string_view y_view = view.substr(index + 1);
+
+        float x = parse_float(x_view);
+        float y = parse_float(y_view);
+        if (std::isinf(x) || std::isinf(y)) {
+            continue;
+        }
+
+        result.emplace_back(x, y);
     }
 
-    fclose(file);
-
-    return scan;
+    return result;
 }
