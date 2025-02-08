3c4e8e39d27a950860138865b67cf396f727dba9
Yey007 <55263178+Yey007@users.noreply.github.com>
2025-02-08 12:09:30 -0500

    Don't use get_chars

--- common/parse_scan.cpp
+++ common/parse_scan.cpp
@@ -1,21 +1,12 @@
 #include "parse_scan.h"
-#include <charconv>
 #include <cmath>
 #include <cstddef>
+#include <cstdlib>
 #include <fstream>
 #include <stdexcept>
 #include <string_view>
 #include <vector>
 #include "icp/geo.h"
-
-float parse_float(std::string_view view) {
-    float f;
-    auto [_, ec] = std::from_chars(view.data(), (view.data() + view.size()), f);
-    if (ec != std::errc()) {
-        throw std::runtime_error("failed to read lidar scan: invalid value");
-    }
-    return f;
-}
 
 std::vector<icp::Vector> parse_lidar_scan(std::string path) {
     std::ifstream in(path);
@@ -33,8 +24,9 @@ std::vector<icp::Vector> parse_lidar_scan(std::string path) {
         std::string_view x_view = view.substr(0, index);
         std::string_view y_view = view.substr(index + 1);
 
-        float x = parse_float(x_view);
-        float y = parse_float(y_view);
+        char* end = nullptr;
+        double x = std::strtod(x_view.cbegin(), &end);
+        double y = std::strtod(y_view.cbegin(), &end);
         if (std::isinf(x) || std::isinf(y)) {
             continue;
         }
